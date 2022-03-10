1395bcfa9ab30f545d030afaaa43b74046cd98db
pc <pc@192.168.0.21>
2022-03-09 23:56:00 -0600

    Primer Commit

---
+++ src/helpers/Ingenieria.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helpers;
+
+/**
+ *
+ * @author pc
+ */
+public abstract class Ingenieria {
+    
+   abstract String getFacultad();
+}
